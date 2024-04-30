61679ed0303f06a01a74c3fab20e02900b4c8ec9
dharmendra1104 <dharmendra11042@gmail.com>
2024-04-30 21:50:19 +0300

    Update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const addButton = document.querySelector('.todo-input button');
 const itemInput = document.getElementById('todo-input');
 const todo = document.querySelector('.todos ul');
 const clearButton = document.querySelector('.clear');
+const mobClearButton = document.querySelector('.mob-clear');
 const filters = document.querySelectorAll('.filters input[type="radio"]');
 
 /* Change Theme */
@@ -42,19 +43,31 @@ function addItem() {
         todo.appendChild(item);
         itemInput.value = '';
         updateCount(1);
+        item.querySelector('.remove').addEventListener('click', removeItem);
     }
+}
+
+/* Remove items */
+function removeItem(event) {
+    const listItem = event.target.closest('li');
+    listItem.remove();
+    updateCount(-1);
 }
 
 /*count items */
 const itemCount = document.querySelector('.count span');
+const mobItemCount = document.querySelector('.mob-count span');
+
 function updateCount(num) {
     const currentCount = +itemCount.innerText;
     const newCount = currentCount + num;
     itemCount.innerText = newCount >= 0 ? newCount : 0;
+    mobItemCount.innerText = newCount >= 0 ? newCount : 0;
 }
 
 /*clear complete items */
 clearButton.addEventListener('click', clearCompletedTasks);
+mobClearButton.addEventListener('click', clearCompletedTasks);
 function clearCompletedTasks() {
     const completedItems = todo.querySelectorAll('.checkbox:checked');
     const numCompleted = completedItems.length;
@@ -81,3 +94,6 @@ filters.forEach(filter => {
         });
     });
 });
+
+
+
